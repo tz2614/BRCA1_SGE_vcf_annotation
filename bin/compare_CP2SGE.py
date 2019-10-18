c949ab45ee3bb78cd05236803e0afae1afa01f1b
pushpad <pushpad>
2019-10-18 19:17:01 +0100

    running compare_CP2SGE.py to get a list of CP samples

--- bin/compare_CP2SGE.py
+++ bin/compare_CP2SGE.py
@@ -6,11 +6,13 @@ import subprocess
 import datetime
 import pprint as pp
 import vcf
+import glob
+import json
 
 """create a script that checks which CP samples contain BRCA1 variants in the BRCA1_SGE excel spreadsheet"""
 
 
-def create_BRCA1_SGE_dict(csv_file):
+def create_BRCA1_SGE_dict(csv_file, output=None):
 
 	#create a dictionary containig containing the CHROM, POS, REF and ALT
 
@@ -64,60 +66,82 @@ def create_BRCA1_SGE_dict(csv_file):
 					BRCA1_SGE_dict[CHROM][POS][REF].append(ALT)
 
 	
-	print ("BRCA1_SGE_dict generated")
+	#print ("BRCA1_SGE_dict generated")
+
+	pp.pprint(BRCA1_SGE_dict)
+	if output:
+		with open(output, "a") as output_file:
+			json.dump(BRCA1_SGE_dict, output_file)
 	return BRCA1_SGE_dict
 
 def get_list_of_C01_vcfs(runfolder):
 
 	vcf_list = []
 
-	for (dirpath, dirnames, filenames) in os.walk(runfolder):
-		for filename in filenames:
-			if filename.endswith(".vcf") and filename.startswith("C01") and filename not in vcf_list:
-				vcf_list.extend(filenames)
-			else:
-				continue
+	filenames = glob.glob("%s*/vcfs/C01*.vcf" % runfolder)
+	for filename in filenames:
+		#print filename
+		if ".vcf" in filename and "C01" in filename and filename not in vcf_list:
+			vcf_list.append(filename)
+		else:
+			continue
 				
-	print (sorted(vcf_list))
+	#print (sorted(vcf_list))
 	return sorted(vcf_list)
 
-def compare_BRCA1_SGE_to_vcf(vcf_list, BRCA1_SGE_dict):
+def compare_BRCA1_SGE_to_vcf(vcf_list, BRCA1_SGE_dict, output=None):
 
 	"""check which CP file contain variants from the supplementary table 1.csv file"""
 
-	SGE_positive_vcfs = []
+	C01_sample_SGE_dict = {}
 
 	for vcf_file in vcf_list:
 
 		# check each vcf record against the BRCA1_SGE_dict
 	
 		vcf_data = vcf.Reader(open(vcf_file, "rb"))
+		
+		if vcf_file not in C01_sample_SGE_dict.keys():
+			vcf_records = []
 
 		print ("analysing {}".format(vcf_file))
 
 		for record in vcf_data:
 			try:
-				if BRCA1_SGE_dict[record.CHROM][record.POS][record.REF] == record.ALT[0]:
-					print ("{} contain the following BRCA1_SGE_variants:".format(vcf_file))
-					print (record)
-
+				if not record.CHROM in BRCA1_SGE_dict.keys():
+					continue
+				if not record.POS in BRCA1_SGE_dict[record.CHROM].keys():
+					continue
+				if not record.REF in BRCA1_SGE_dict[record.CHROM][record.POS].keys():
+					continue
+				if record.ALT[0] in BRCA1_SGE_dict[record.CHROM][record.POS][record.REF].values():
+					vcf_records.append(record)
+					print "%s contain the following BRCA1_SGE_variants:" % vcf_file
+					print record
+				else:
+					continue
+					 				
 			except KeyError:
 				continue
 
-		SGE_positive_vcfs.append(vcf_file)
+		C01_sample_SGE_dict[vcf_file] = vcf_records
+		if output:
+			with open (output, "a") as output_file:
+				json.dump(C01_sample_SGE_dict[vcf_file], output_file)
 
-	return SGE_positive_vcfs
+	print C01_sample_SGE_dict.keys()
+	return output_file
 
-def main(runfolder, csv_file):
+def main(runfolder, csv_file, SGE_dict_file, C01sample_file):
 
 	# check that the annotated_vcf and excel_table exist as a data source
 	assert os.path.exists(csv_file), "{} DO NOT exists".format(csv_file)
 
-	BRCA1_SGE_dict = create_BRCA1_SGE_dict(csv_file)
+	BRCA1_SGE_dict = create_BRCA1_SGE_dict(csv_file, SGE_dict_file)
 	vcf_list = get_list_of_C01_vcfs(runfolder)
-	compare_BRCA1_SGE_to_vcf(vcf_list, BRCA1_SGE_dict)
+	compare_BRCA1_SGE_to_vcf(vcf_list, BRCA1_SGE_dict, C01sample_file)
 
 if __name__ == "__main__":
-	main(sys.argv[1], sys.argv[2])
+	main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
-	"""sys.argv1 = /path/to/CP runfolder, sys.argv2 = supplementary table 1.csv"""
+	"""sys.argv1 = /path/to/CP runfolder, sys.argv2 = supplementary table 1.csv, sys.argv[3] = BRCA1_SGE_file, sys.argv[4] = C01sample_file"""
